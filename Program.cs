35f702f9219dabed2480d0e4991a0e4acd381ab0
ZhanMakh <103171669+ZhanMakh@users.noreply.github.com>
2022-04-16 14:12:34 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Домашнее задание № 2
+Console.WriteLine("Введите два числа: ");
+int numberA = 5;
+int numberB = 7;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+if (numberA>=numberB)
+{
+    Console.Write("Максимальное число - ");
+    Console.WriteLine(numberA+" ");
+    Console.Write("Минимальное число - ");
+    Console.WriteLine(numberB+" ");
+} else
+{
+    Console.Write("Максимальное число - ");
+    Console.Write(numberB+" ");
+    Console.Write("Минимальное число - ");
+    Console.WriteLine(numberA+" ");
+}
