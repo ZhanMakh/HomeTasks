56a5e66b086b6326f7fef63ce4a24fc5ce6987ca
ZhanMakh <103171669+ZhanMakh@users.noreply.github.com>
2022-04-16 17:26:50 +0600

    Выполнена задача № 8

---
+++ Program.cs_Task08.cs
@@ -0,0 +1,10 @@
+﻿// Домашнее задание № 8
+Console.WriteLine("Введите число ");
+int number = 8;
+Console.WriteLine(number);
+int index = 0;
+while(index<number)
+{
+    index=index+2;
+    Console.Write(index+" ");
+}
