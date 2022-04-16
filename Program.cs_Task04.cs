3694bc521bf4b37ac746bc2d9b192b64db30b9a9
ZhanMakh <103171669+ZhanMakh@users.noreply.github.com>
2022-04-16 16:58:44 +0600

    Выполнена задача № 4

---
+++ Program.cs_Task04.cs
@@ -0,0 +1,14 @@
+﻿// Домашнее задание № 4
+Console.WriteLine("Введите три числа: ");
+int numA = 2;
+int numB = 13;
+int numC = 23;
+Console.WriteLine(numA);
+Console.WriteLine(numB);
+Console.WriteLine(numC);
+
+int max = numA;
+if (numB>max) max = numB;
+if (numC>max) max = numC;
+Console.Write("Максимальное число - ");
+Console.WriteLine(max);
