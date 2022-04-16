e5a33dc5d858751258bbaa59210cb15e0ac140a4
ZhanMakh <103171669+ZhanMakh@users.noreply.github.com>
2022-04-16 17:08:13 +0600

    Выполнена задача № 6

---
+++ Program.cs_Task06.cs
@@ -0,0 +1,6 @@
+﻿// Домашнее задание № 6
+Console.WriteLine("Введите число ");
+int number = 5;
+Console.WriteLine(number);
+if (number%2 == 0) Console.WriteLine("Это четное число");
+else Console.WriteLine("Это нечетное число");
